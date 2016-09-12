f26471df050c0b8e0fef645027b4601536c4ba8c
BruceHib <huangyoupeng93@gmail.com>
2016-09-12 13:00:01 -0500

    Add files via upload
    second version of 2014 crawler. the first one can't run in my macbook. the problem seems to be the protocol. I try another computer with wins 10 OS and it works. I didn't get the problem yet. 
    
    this version works too.

---
+++ test2wins.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import urllib2
+urlf = urllib2.urlopen('https://www.usenix.org/conference/usenixsecurity14/technical-sessions'
+)
+# check by : print page
+soup = BeautifulSoup(urlf.read())
+link = soup.findAll('h2', attrs={'class':'node-title clearfix'})
+
+urls = []
+for n in link:
+	url = 'https://www.usenix.org' + str(n.a['href'])
+	urls.append(url)
+techurl = [l for l in urls if "technical-sessions/presentation" in str(l)]
+
+def AbsFinder(testurl):
+    testurlf = urllib2.urlopen(testurl)
+    testsoup = BeautifulSoup(testurlf.read())
+    link = testsoup.findAll('div', {'class': 'field-item odd'})
+    testorder = len(link) - 1
+    return link[testorder].text
+
+abss = []
+for m in techurl:
+    abs = AbsFinder(m)
+    abss.append(abs)
+
