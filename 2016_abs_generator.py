79755f2c289a79a6c639dac353cf003c7a9861e3
BruceHib <huangyoupeng93@gmail.com>
2016-09-12 23:41:02 -0500

    Update 2016_abs_generator.py
    update urlsFinder and titlesFinder to find urls and titles as sets. can work with dictionary.

--- 2016_abs_generator.py
+++ 2016_abs_generator.py
@@ -23,10 +23,35 @@ def absFinder(hurl):
 	testorder = len(link) - 1 
 	return link[testorder].text
 
+def urlsFinder(urlf):
+    urlf = urllib2.urlopen(urlf)
+    soupf = BeautifulSoup(urlf.read())
+    linkf = soupf.findAll('h2', attrs={'class': 'node-title clearfix'})
+    urls = []
+    for n in linkf:
+        url = 'https://www.usenix.org' + str(n.a['href'])
+        urls.append(url)
+    techurl = [l for l in urls if "technical-sessions/presentation" in str(l)]
+    return techurl
+
+def titleFinder(furl):
+	furl = urllib2.urlopen(furl)
+	fsoup = BeautifulSoup(furl.read())
+	flink = fsoup.findAll('h2', attrs={'class':'node-title clearfix'})
+	titles = []
+	for n in flink:
+		url = 'https://www.usenix.org' + str(n.a['href'])
+		if "technical-sessions/presentation" in url:
+			text = n.text
+			titles.append(text)
+	return titles
+
 if __name__=="__main__":
 
 #input url need to analysis
 url = 'https://www.usenix.org/conference/usenixsecurity14/technical-sessions'
+urls = urlsFinder(url)
+titles = titleFinder(url)
 abss = urlFinder(url)
 abss
 
