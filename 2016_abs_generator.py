ebe9ced118b2a33cc3af324c568fddc229b8ddb3
BruceHib <huangyoupeng93@gmail.com>
2016-09-12 22:52:40 -0500

    Update 2016_abs_generator.py

--- 2016_abs_generator.py
+++ 2016_abs_generator.py
@@ -2,20 +2,20 @@ from bs4 import BeautifulSoup
 import urllib2
 
 
-def UrlFinder(furl):
+def urlFinder(furl):
 	furl = urllib2.urlopen(str(furl))
 	fsoup = BeautifulSoup(furl.read())
-	flink = soup.findAll('h2', attrs={'class':'node-title clearfix'})
+	flink = fsoup.findAll('h2', attrs={'class':'node-title clearfix'})
 	abss = {}
 	for n in flink:
 		url = 'https://www.usenix.org' + str(n.a['href'])
 		if "technical-sessions/presentation" in url:
-			abs = AbsFinder(url)	
+			abs = absFinder(furl)	
 			text = str(n.text)
 			abss[text] = abs
 	return abss
 
-def AbsFinder(hurl):
+def absFinder(hurl):
 	hurl = urllib2.urlopen(hurl)
 	hsoup = BeautifulSoup(hurl.read())
 	link = hsoup.findAll('div', {'class': 'field-item odd'})
@@ -27,6 +27,6 @@ if __name__=="__main__":
 
 #input url need to analysis
 url = 'https://www.usenix.org/conference/usenixsecurity14/technical-sessions'
-abss = UrlFinder(url)
+abss = urlFinder(url)
 abss
 
